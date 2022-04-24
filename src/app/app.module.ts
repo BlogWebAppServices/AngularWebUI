c739ad785919a7d04b47128b16a7a9b5db4be508
abdussamet topal <85809119+astopaal@users.noreply.github.com>
2022-04-24 17:13:54 +0300

    first
    first-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+import { AuthorProfileComponent } from './author-profile/author-profile.component';
+import { ContactComponent } from './contact/contact.component';
+import { ArticleComponent } from './home/article/article.component';
+import { ArticleDetailComponent } from './home/article-detail/article-detail.component';
+import { CarouselComponent } from './slider/carousel/carousel.component';
+import { FooterComponent } from './slider/footer/footer.component';
+import { HeaderComponent } from './slider/header/header.component';
+import { NavbarComponent } from './slider/navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    AdminComponent,
+    AuthorProfileComponent,
+    ContactComponent,
+    ArticleComponent,
+    ArticleDetailComponent,
+    CarouselComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
